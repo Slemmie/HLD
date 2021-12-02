491da3aae3c5aeb6f039e8c20e68c0056dd04781
Slemmie <thorvejeneriksen@gmail.com>
2021-12-02 23:48:56 +0100

    clear paths

--- save_tests.sh
+++ save_tests.sh
@@ -1,33 +1,38 @@
+if [ -d "./tests/" ]; then
+	rm -rf ./tests/
+fi
+
+mkdir -p tests/n_less_100/
+mkdir -p tests/n_less_1000/
+mkdir -p tests/n_less_10000/
+mkdir -p tests/n_less_100000/
+mkdir -p tests/n_less_1000000/
+
 echo "tests: n < 100"
-rm ./tests/n_less_100/*
 for (( i=0; i<=10; i++ ))
 do
 	./gen 100 > "tests/n_less_100/test_$i"
 done
 
 echo "tests: n < 1000"
-rm ./tests/n_less_1000/*
 for (( i=0; i<=10; i++ ))
 do
 	./gen 1000 > "tests/n_less_1000/test_$i"
 done
 
 echo "tests: n < 10000"
-rm ./tests/n_less_10000/*
 for (( i=0; i<=10; i++ ))
 do
 	./gen 10000 > "tests/n_less_10000/test_$i"
 done
 
 echo "tests: n < 100000"
-rm ./tests/n_less_100000/*
 for (( i=0; i<=10; i++ ))
 do
 	./gen 100000 > "tests/n_less_100000/test_$i"
 done
 
 echo "tests: n < 1000000"
-rm ./tests/n_less_1000000/*
 for (( i=0; i<=10; i++ ))
 do
 	./gen 1000000 > "tests/n_less_1000000/test_$i"
